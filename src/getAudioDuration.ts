5e9ac29669cd4f3c312befd08501d9dc79cad684
yamashita <hirotsugu_yamashita@dwango.co.jp>
2017-01-27 17:06:02 +0900

    support mp4

--- src/getAudioDuration.ts
+++ src/getAudioDuration.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 import * as aacDuration from "aac-duration";
 import * as musicMetaData from "musicmetadata";
+import { mp4Inspector } from "thumbcoil";
 
 export function getAudioDuration(filepath: string): Promise<number> {
 	var ext = path.extname(filepath);
@@ -13,6 +14,8 @@ export function getAudioDuration(filepath: string): Promise<number> {
 		} catch (e) {
 			return Promise.reject<number>(e);
 		}
+
+		console.log(path.basename(filepath), d);
 		return Promise.resolve(d);
 	case ".ogg":
 		return new Promise((resolve, reject) => {
@@ -20,9 +23,23 @@ export function getAudioDuration(filepath: string): Promise<number> {
 				if (err) {
 					return reject(err);
 				}
+				console.log(path.basename(filepath), metadata.duration);
 				resolve(metadata.duration);
 			});
 		});
+	case ".mp4":
+		var n: number;
+		try {
+			var data = fs.readFileSync(filepath);
+			var moov = mp4Inspector.inspect(data).filter((o: any) => o.type === "moov")[0]; // 必須BOXなので必ず1つある
+			var mvhd = moov.boxes.filter((o: any) => o.type === "mvhd")[0]; // MoVie HeaDer。moov直下の必須フィールドなので必ず1つある
+			n = mvhd.duration / 1000;
+		} catch (e) {
+			return Promise.reject<number>(e);
+		}
+
+		console.log(path.basename(filepath), n);
+		return Promise.resolve(n);
 	default:
 		return Promise.reject<number>(new Error("Unsupported format: " + ext));
 	}
